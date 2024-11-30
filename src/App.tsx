53c3dcdc3662af9e79db779f91eb6ba16295bd8b
Matthew Vance <mavance44@gmail.com>
2024-11-29 23:37:00 -0500

    add textarea and image examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { date, z } from "zod";
+import { z } from "zod";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { jsPDF } from "jspdf";
+import { faker } from "@faker-js/faker";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -24,159 +25,185 @@ import {
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import imgUrl from "./assets/img.png";
 
 const formSchema = z
   .object({
     name: z.string().min(1),
-    age: z.coerce.number().positive().int(),
-    date: date(),
+    number: z.coerce.number().positive().int(),
+    date: z.date(),
+    freeText: z.string().optional(),
   })
   .required();
 type FormSchema = z.infer<typeof formSchema>;
 
 function App() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: faker.person.fullName(),
+      number: faker.number.int({ min: 1, max: 100 }),
+      date: new Date(),
+      freeText: faker.lorem.paragraphs(10),
+    },
+  });
+
   const [pdfDataUrl, setPdfDataUrl] = useState<string>();
 
   function generatePdf(formData?: Partial<FormSchema>) {
-    const doc = new jsPDF();
+    const doc = new jsPDF({ format: "legal" });
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+    const middle = pageWidth / 2;
 
-    doc.setFont("Helvetica", "bold");
-    doc.setFontSize(16);
-    doc.text("Generated PDF", 20, 20);
-    doc.setFont("Helvetica", "normal");
-    doc.setFontSize(12);
-    doc.text(`Name: ${formData?.name ?? ""}`, 20, 40);
-    doc.text(`Age: ${formData?.age?.toString() ?? ""}`, 20, 50);
-    doc.text(
-      `Date: ${formData?.date ? format(formData.date, "PPP") : ""}`,
-      20,
-      60
-    );
+    doc
+      .line(middle, pageHeight, middle, 0)
+      .setFont("Helvetica", "bold")
+      .setFontSize(16)
+      .text("Generated PDF", 20, 20)
+      .addImage(imgUrl, "PNG", middle - 10, 80, 20, 20)
+      .setFont("Helvetica", "normal")
+      .setFontSize(12)
+      .text(`Name: ${formData?.name ?? ""}`, 20, 40)
+      .text(`Number: ${formData?.number?.toString() ?? ""}`, 20, 50)
+      .text(
+        `Date: ${formData?.date ? format(formData.date, "PPP") : ""}`,
+        20,
+        60
+      )
+      .text(`Free text: ${formData?.freeText ?? ""}`, 20, 70, {
+        maxWidth: pageWidth - 40,
+      })
+      .text("This is a footer", 20, pageHeight - 20);
 
     const pdfUrl = doc.output("dataurlstring");
+    console.log(pdfUrl);
     setPdfDataUrl(pdfUrl);
   }
-
-  useEffect(() => {
-    generatePdf();
-  }, []);
-
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-  });
 
   function onSubmit(values: FormSchema) {
     generatePdf(values);
   }
 
-  function TypographyH1({ children }: { children: React.ReactNode }) {
-    return (
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        {children}
-      </h1>
-    );
-  }
-
-  function TypographyH2({ children }: { children: React.ReactNode }) {
-    return (
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        {children}
-      </h2>
-    );
-  }
+  useEffect(() => {
+    generatePdf(form.getValues());
+  }, [form]);
 
   return (
-    <div className="container mx-auto px-4 flex flex-col gap-4">
-      <header>
-        <TypographyH1>Header</TypographyH1>
-      </header>
-      <main className="flex gap-8">
-        <section className="w-1/3">
-          <TypographyH2>Form side</TypographyH2>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Name" {...field} />
-                    </FormControl>
-                    <FormDescription>Sample text input.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="age"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Age</FormLabel>
-                    <FormControl>
-                      <Input type="number" placeholder="Age" {...field} />
-                    </FormControl>
-                    <FormDescription>Sample number input.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="date"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Date of birth</FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant={"outline"}
-                            className={cn(
-                              "w-[240px] pl-3 text-left font-normal",
-                              !field.value && "text-muted-foreground"
-                            )}
-                          >
-                            {field.value ? (
-                              format(field.value, "PPP")
-                            ) : (
-                              <span>Pick a date</span>
-                            )}
-                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          disabled={(date) =>
-                            date > new Date() || date < new Date("1900-01-01")
-                          }
-                          initialFocus
-                        />
-                      </PopoverContent>
-                    </Popover>
-                    <FormDescription>Sample date input.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
-        </section>
-        <section className="w-2/3 flex flex-col">
-          <TypographyH2>Preview side</TypographyH2>
+    <div className="container mx-auto px-2">
+      <main className="grid grid-cols-3 gap-4 min-h-screen">
+        <div className="col-span-1 flex flex-col gap-4">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Form to PDF Prototype
+          </h1>
+          <section>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Name" {...field} />
+                      </FormControl>
+                      <FormDescription>Sample text input.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="number"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Number</FormLabel>
+                      <FormControl>
+                        <Input type="number" placeholder="Number" {...field} />
+                      </FormControl>
+                      <FormDescription>Sample number input.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel>Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-[240px] pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormDescription>Sample date input.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="freeText"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Free text</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Free text" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        A sample textarea input.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex gap-2">
+                  <Button type="submit">Generate</Button>
+                </div>
+              </form>
+            </Form>
+          </section>
+        </div>
+        <section className="col-span-2">
           {pdfDataUrl && (
-            <iframe
-              src={pdfDataUrl}
-              title="PDF Preview"
-              className="w-full h-full"
-            />
+            <object
+              data={pdfDataUrl + "#view=FitV&navpanes=0"}
+              type="application/pdf"
+              width="100%"
+              height="100%"
+            ></object>
           )}
         </section>
       </main>
